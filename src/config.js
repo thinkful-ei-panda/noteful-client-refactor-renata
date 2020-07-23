52640e394c9ab53ca986a68a2832691c3180e2a9
seraphyne <renatafd717@gmail.com>
2020-07-23 15:55:50 -0500

    rdy for deploy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
+let BASE_URL;
 
-    export const API_KEY= process.env.REACT_APP_API_KEY;
-    export const BASE_URL= 'https://localhost:8000' || process.env.REACT_APP_BASE_URL;
+if(process.env.NODE_ENV === 'development'){
+    BASE_URL='http://localhost:8000';
+}else if(process.env.NODE_ENV === 'production'){
+    BASE_URL=process.env.REACT_APP_BASE_URL
+}
+
+export default BASE_URL;
+export const API_KEY= process.env.REACT_APP_API_KEY;
