acabc50191cfa4ca462f0e29ab23681c14f81dfe
seraphyne <renatafd717@gmail.com>
2020-07-24 15:04:04 -0500

    console.log added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import FullNoteSide from './FullNoteSide/FullNoteSide';
 import './App.css';
 import { API_KEY } from './config';
 import BASE_URL from './config';
-
+console.log(BASE_URL);
 
 //TO RUN Noteful JSON Server
 //git clone https://github.com/tomatau/noteful-json-server
